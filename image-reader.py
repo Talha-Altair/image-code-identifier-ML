4251f8420f240a87e246ee7399698c753720e805
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-06-30 15:57:44 +0530

    base files

---
+++ image-reader.py
@@ -0,0 +1,34 @@
+import cv2
+from pyzbar.pyzbar import decode
+import os
+
+def BarcodeReader(image):
+    
+    img = cv2.imread(image)
+
+    detectedBarcodes = decode(img)
+    
+    if not detectedBarcodes:
+        return ("Barcode Not Detected in this image")
+    else:
+        for barcode in detectedBarcodes:
+            (x, y, w, h) = barcode.rect
+            
+            cv2.rectangle(img, (x-10, y-10),(x + w+10, y + h+10),(255, 0, 0), 2)
+
+            if barcode.data!="":
+                return (barcode.data)
+            else:
+                return "data not clear"
+
+if __name__ == "__main__":
+
+    all_images = os.listdir('static/')
+
+    for image in all_images:
+
+        print(f"image_name:{image}")
+
+        data = BarcodeReader(f'static/{image}')
+
+        print(f'data: {data}\n')
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Barcode Reader
+
+Place all Required Images inside Static Folder
+
+```bash
+
+pip install -r "requirements.txt"
+
+```
+
+```bash
+
+python image-reader.py
+
+```
